b065cb420fbbb61206fc30aa05091754584887e2
hos7amdy <hossam7amdy@gmail.com>
2024-07-11 18:27:03 +0300

    chore: Add Dockerfile and docker-compose.yml for local development and production deployment

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+  });
+
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+
   await app.listen(3000);
 }
+
 bootstrap();
