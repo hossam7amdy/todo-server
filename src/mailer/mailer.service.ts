fb8d4c5a73e8f616de30fb3bec7276b677ca98a2
Hossam Hamdy <hossamhamdy117@gmail.com>
2025-09-21 11:46:04 +0300

    feat: Update email sender configuration to use EMAIL_SENDER environment variable

--- src/mailer/mailer.service.ts
+++ src/mailer/mailer.service.ts
@@ -21,7 +21,7 @@ export class MailerService {
 
   async sendMail(to: string, subject: string, html: string): Promise<void> {
     const mailOptions = {
-      from: `"Todo App" <${process.env.EMAIL_USER}>`,
+      from: `"Todo App" <${process.env.EMAIL_SENDER}>`,
       to,
       subject,
       html,
